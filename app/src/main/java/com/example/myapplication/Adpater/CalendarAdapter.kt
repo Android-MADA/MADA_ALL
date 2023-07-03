f54b46a17b1e64be1501679e5d412ed7fbde3fdf
Beomsu <qjatn0521@kw.ac.kr>
2023-07-03 15:59:09 +0900

    0703 (버루) UI 수정

--- app/src/main/java/com/example/myapplication/Adpater/CalendarAdapter.kt
+++ app/src/main/java/com/example/myapplication/Adpater/CalendarAdapter.kt
@@ -1,15 +1,23 @@
 package com.example.myapplication.Adpater
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.Listener.OnItemListener
+import com.example.myapplication.CalenderFuntion.CalendarUtil
 import com.example.myapplication.R
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Date
 
-class CalendarAdapter(private val dayList: ArrayList<String>, private  val onItemListener: OnItemListener)
+class CalendarAdapter(private val dayList: ArrayList<Date>)
         : RecyclerView.Adapter<CalendarAdapter.ItemViewHolder>()  {
+    var m = LocalDate.now().monthValue
+    var y = LocalDate.now().year
+    var d = LocalDate.now().dayOfMonth
 
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textDay : TextView = itemView.findViewById(R.id.textDay)
@@ -21,11 +29,27 @@ class CalendarAdapter(private val dayList: ArrayList<String>, private  val onIte
     }
     //데이터 설정
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        var day = dayList[holder.adapterPosition]
-        holder.textDay.text = day
-        holder.itemView.setOnClickListener {
-            onItemListener.onItemClick(day)
+        var monthDate = dayList[holder.adapterPosition]
+        var dateCalendar = Calendar.getInstance()
+        dateCalendar.time = monthDate
+        var dayNo = dateCalendar.get(Calendar.DAY_OF_MONTH)
+        holder.textDay.text = dayNo.toString()
+        var iYear = dateCalendar.get(Calendar.YEAR)
+        var iMonth = dateCalendar.get(Calendar.MONTH) + 1
+        var iDay = dateCalendar.get(Calendar.DAY_OF_MONTH)
+        if(CalendarUtil.selectedDate.monthValue != iMonth) {
+            holder.textDay.setTextColor(Color.LTGRAY)
         }
+        if(iYear == y && iMonth == m && iDay == d) {
+            holder.itemView.setBackgroundResource(R.drawable.calender_today)
+            holder.textDay.setTextColor(Color.WHITE)
+        }
+
+        holder.itemView.setOnClickListener {
+            var yearMonDay = "$iYear 년 $iMonth 월 $iDay 일"
+            Toast.makeText(holder.itemView.context, yearMonDay,Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     override fun getItemCount(): Int {
