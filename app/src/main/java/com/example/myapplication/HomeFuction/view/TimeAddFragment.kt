831a8204048ab6b6169e0e2e6d286b846327d002
Minji Kang <1117minsky@hufs.ac.kr>
2023-07-19 16:02:05 +0900

    [mindy] feat. timeadd clicklistener

--- app/src/main/java/com/example/myapplication/HomeFuction/view/TimeAddFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFuction/view/TimeAddFragment.kt
@@ -1,13 +1,13 @@
 package com.example.myapplication.HomeFuction.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.HomeFragmentTimeAddBinding
 
@@ -23,7 +23,7 @@ class TimeAddFragment : Fragment() {
     ): View? {
         val binding : HomeFragmentTimeAddBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment_time_add, container, false)
 
-        val iv_color = binding.ivHomeTimeColor
+        val ivColor = binding.ivHomeTimeColor
         val colorSelector = binding.layoutColorSelector
 
         val timeStart = binding.tvHomeTimeStart
@@ -31,7 +31,24 @@ class TimeAddFragment : Fragment() {
         val timepickerStart = binding.homeTimepickerStart
         val timepickerEnd = binding.homeTimepickerEnd
 
-        iv_color.setOnClickListener {
+        val btnSubmit = binding.btnHomeTimeAddSubmit
+        val btnDelete = binding.btnHomeTimeEditDelete
+
+        //파라미터가 전달된다면(생성이 아니라 수정이라면)
+//        if(){
+//            //1. 등록 text를 수정 text 로 변경하기
+//            btnSubmit.text = "수정"
+//            //2. 삭제하기 btn 활설화시키기
+//            btnDelete.isVisible = true
+//            //3. 받아온 파라미터들을 알맞은 장소에 넣기
+//            //4. 이전 시간표 데이터는 삭제하기
+//        }
+
+
+        //색상 선택창
+        // radio group 처리하기
+        // 클릭 시 아이콘 색도 바꾸기
+        ivColor.setOnClickListener {
             if(colorSelector.isVisible){
                 colorSelector.isGone = true
             }
@@ -40,7 +57,6 @@ class TimeAddFragment : Fragment() {
             }
         }
 
-        // 열려 있으면 닫고, 다른 게 열러 있으면 다른 열린 거 닫고 열고, 안 열려 있으면 그냥 열고
         timeStart.setOnClickListener {
             if(timepickerStart.isVisible){
                 timepickerStart.isGone = true
@@ -61,12 +77,49 @@ class TimeAddFragment : Fragment() {
             }
         }
 
+
+        //날짜 선택 창
         timepickerStart.setOnTimeChangedListener { view, hourOfDay, minute ->
-            timeStart.text = "${hourOfDay}:${minute}"
+            val AM_PM_start : String
+            AM_PM_start = if (hourOfDay < 12) {
+                "오전"
+            } else {
+                "오후"
+            }
+
+            val hourStart = if (hourOfDay <= 12) {
+                hourOfDay
+            } else {
+                hourOfDay - 12
+            }
+
+            timeStart.text = "${AM_PM_start} ${hourStart}:${minute}"
         }
 
         timepickerEnd.setOnTimeChangedListener { view, hourOfDay, minute ->
-            timeEnd.text = "${hourOfDay}:${minute}"
+            val AM_PM_end : String
+            AM_PM_end = if (hourOfDay < 12) {
+                "오전"
+            } else {
+                "오후"
+            }
+
+            val hourEnd = if (hourOfDay <= 12) {
+                hourOfDay
+            } else {
+                hourOfDay - 12
+            }
+
+
+            timeEnd.text = "${AM_PM_end} ${hourEnd}:${minute}"
+        }
+
+        //등록 btn
+        btnSubmit.setOnClickListener {
+            //1. db에 저장(생성) 또는 수정(수정)
+            // 수정버튼을 누르면 시간표가 생성되는 게 아니라 수정됨
+            // list 만들고 그 안에 파라미터를 받고(데이터를 저장해서) db에 넘기기
+            //2. 시간표 화면으로 이동
         }
 
 
