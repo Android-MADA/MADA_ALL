9917e4c03257e15cb6a627c91d601cbb3df7b904
Minji Kang <1117minsky@hufs.ac.kr>
2023-07-20 01:02:43 +0900

    [mindy] feat. category icon selector 구현

--- app/src/main/java/com/example/myapplication/HomeFuction/view/CategoryAddFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFuction/view/CategoryAddFragment.kt
@@ -1,60 +1,90 @@
 package com.example.myapplication.HomeFuction.view
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.myapplication.HomeFuction.category.HomeCateIconAdapter
 import com.example.myapplication.R
+import com.example.myapplication.databinding.HomeFragmentCategoryAddBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoryAddFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoryAddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    val cateIconArray = ArrayList<Drawable>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.home_fragment_category_add, container, false)
+
+        val binding : HomeFragmentCategoryAddBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment_category_add, container, false)
+        val ivIcon = binding.ivHomeCateIcon
+        val iconAdapter = HomeCateIconAdapter(cateIconArray)
+
+        initArrayList()
+
+        val rvIcon = binding.rvHomeCateIcon
+        var listManager = GridLayoutManager(this.activity, 6)
+        iconAdapter.setItemClickListener(object: HomeCateIconAdapter.OnItemClickListener{
+            override fun onClick(v: View, position: Int) {
+                ivIcon.setImageDrawable(cateIconArray[position])
+                rvIcon.isGone = true
+            }
+        })
+
+        var recyclerList = rvIcon.apply {
+            setHasFixedSize(true)
+            layoutManager = listManager
+            adapter = iconAdapter
+
+        }
+
+        ivIcon.setOnClickListener {
+            if(rvIcon.isVisible){
+                rvIcon.isGone = true
+            }
+            else {
+                rvIcon.isVisible = true
+            }
+        }
+
+
+        //binding.rvHomeCategory.layoutManager = LinearLayoutManager(this.activity)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeAddCategoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryAddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initArrayList(){
+        with(cateIconArray){
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_meal1))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_meal2))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_chat1))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_health))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_phone))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_rest))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_heart))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_study))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_laptop))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_music))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_lightup))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_lightout))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_pen))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_burn))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_plan))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_work))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_mic))
+            cateIconArray.add(resources.getDrawable(R.drawable.ic_home_cate_sony))
+        }
     }
+
 }
