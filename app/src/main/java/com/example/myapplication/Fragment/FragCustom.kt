580f718285ac3159b14d73016c7fe3a45992ecde
YUNSO99 <l01090259914@gmail.com>
2023-07-31 17:24:26 +0900

    custom fragment 수정

--- app/src/main/java/com/example/myapplication/Fragment/FragCustom.kt
+++ app/src/main/java/com/example/myapplication/Fragment/FragCustom.kt
@@ -22,15 +22,10 @@ import com.google.android.material.tabs.TabLayout
 
 class FragCustom : Fragment() {
     lateinit var binding: FragCustomBinding
-    lateinit var colorbinding: CustomColorBinding
-    lateinit var clothbinding: CustomClothBinding
-    lateinit var itembinding: CustomItemBinding
-    lateinit var backgroundBinding: CustomBackgroundBinding
     private lateinit var frag_color: custom_color
     private lateinit var frag_cloth: custom_cloth
     private lateinit var frag_item: custom_item
     private lateinit var frag_background: custom_background
-
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager: ViewPager
     private var tabCurrentIdx = 0
@@ -41,9 +36,7 @@ class FragCustom : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragCustomBinding.inflate(inflater, container, false)
-
         var view = binding.root
-
         var customtabLayout = binding.CustomPagetabLayout
         var viewPager = binding.CustomBottomSheetViewPager
 
@@ -52,34 +45,22 @@ class FragCustom : Fragment() {
         frag_item = custom_item()
         frag_background = custom_background()
 
-
-
-
         tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
                 val inflater1: LayoutInflater = layoutInflater
                 var pos = tab.position
-                var colorTable = colorbinding.customColortable
-                var clothTable = clothbinding.customClothtable
-                var itemTable = itembinding.customItemtable
-                //네비게이션 바처럼 레이아웃을 전환하는 방식
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.CustomBottomSheetTable, custom_color())
-                    .commitAllowingStateLoss()
-
                 when (pos) {
                     0 -> {//인플레이터 사용해서 레이아웃을 연결해주는 방식
-                        inflater1.inflate(R.id.custom_colortable, binding.CustomBottomSheetTable, true)
+                        inflater1.inflate(R.layout.custom_color, binding.CustomBottomSheetTable, true)
                     }
-
                     1 -> {
-                        inflater1.inflate(R.id.custom_clothtable, binding.CustomBottomSheetTable, true)
+                        inflater1.inflate(R.layout.custom_cloth, binding.CustomBottomSheetTable, true)
                     }
                     2 -> {
-                        inflater1.inflate(R.id.custom_itemtable, binding.CustomBottomSheetTable, true)
+                        inflater1.inflate(R.layout.custom_item, binding.CustomBottomSheetTable, true)
                     }
                     3 -> {
-                        inflater1.inflate(R.id.custom_backgroundtable, binding.CustomBottomSheetTable, true)
+                        inflater1.inflate(R.layout.custom_background, binding.CustomBottomSheetTable, true)
                     }
                 }
             }
@@ -95,12 +76,10 @@ class FragCustom : Fragment() {
 
 
 
-        return binding.root
+        return view
     }
 
     private fun changeView(index: Int) {
-
-
 
 
 
