88285879ece005ca0b5a55f3e6932f38bfa88039
qjatn0521 <qjatn0521@kw.ac.kr>
2023-07-31 17:26:04 +0900

    아직 수정중 잠시 커밋

--- app/src/main/java/com/example/myapplication/HomeFunction/time/SampleTimeData.kt
+++ app/src/main/java/com/example/myapplication/HomeFunction/time/SampleTimeData.kt
@@ -2,8 +2,8 @@ package com.example.myapplication.HomeFunction.time
 
 data class SampleTimeData(
     val name :String,
-    val color : Int,
-    val startTime : String,
-    val endTime : String,
-    val date : String
+    val color : Int
+    //val startTime : String,
+    //val endTime : String,
+    //val date : String
 )
--- app/src/main/java/com/example/myapplication/HomeFunction/view/TimeAddFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFunction/view/TimeAddFragment.kt
@@ -13,6 +13,7 @@ import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.myapplication.HomeFunction.view.viewpager2.HomeViewpagerTimetableFragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.HomeFragmentTimeAddBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -32,15 +33,25 @@ class TimeAddFragment : Fragment() {
         val ivColor = binding.ivHomeTimeColor
         val colorSelector = binding.layoutColorSelector
 
-        val timeStart = binding.tvHomeTimeStart
-        val timeEnd = binding.tvHomeTimeEnd
-        val timepickerStart = binding.homeTimepickerStart
-        val timepickerEnd = binding.homeTimepickerEnd
+        val times = arrayOf<TextView>(
+            binding.tvHomeTimeStart,
+            binding.tvHomeTimeEnd
+        )
+        val timepicker = binding.timePicker
+        val ticker1 = binding.numberPicker1
+        val ticker2 = binding.numberPicker2
+        val ticker3 = binding.numberPicker3
 
         val btnSubmit = binding.btnHomeTimeAddSubmit
         val btnDelete = binding.btnHomeTimeEditDelete
 
         val btnBack = binding.ivHomeAddTimeBack
+        // 데이터 받기
+        val receivedData = arguments?.getSerializable("pieChartDataArray") as  Array<HomeViewpagerTimetableFragment.PieChartData>?
+        if( receivedData != null) {
+
+        }
+
 
         //파라미터가 전달된다면(생성이 아니라 수정이라면)
 //        if(){
@@ -124,50 +135,86 @@ class TimeAddFragment : Fragment() {
         }
 
 
+        var scheduleSelect = 0
+        val data1 = arrayOf("오전", "오후")
+        val data2 = arrayOf("00","05","10","15","20","25","30","35","40","45","50","55")
+        val regex = """\s*(오전|오후)\s+(\d{1,2}):(\d{2})\s*""".toRegex()
 
-        //시간 선택
-        fun timeClick(hour : Int, minute : Int, view : TextView){
-            val AM_PM_start : String
-            AM_PM_start = if (hour < 12) {
-                "오전"
+        times[0].setOnClickListener {
+            times[0].setBackgroundResource(R.drawable.calendar_prebackground)
+            times[1].setBackgroundColor(Color.TRANSPARENT)
+            timepicker.isVisible = true
+            scheduleSelect=0
+            val matchResult = regex.find(times[0].text.toString())
+            if (matchResult != null) {
+                val (ampm, hour, minute) = matchResult.destructured
+
+                if(ampm=="오전") {
+                    ticker1.value=0
+                }
+                else {
+                    ticker1.value=1
+                }
+
+                ticker2.value=hour.toInt()
+                ticker3.value=minute.toInt()/5
             } else {
-                "오후"
+                ticker1.value=0
+                ticker2.value=10
+                ticker3.value=0
             }
+        }
 
-            val hourStart = if (hour <= 12) {
-                hour
+        times[1].setOnClickListener {
+            times[1].setBackgroundResource(R.drawable.calendar_prebackground)
+            times[0].setBackgroundColor(Color.TRANSPARENT)
+            timepicker.isVisible = true
+            scheduleSelect=1
+            val matchResult = regex.find(times[1].text.toString())
+            if (matchResult != null) {
+                val (ampm, hour, minute) = matchResult.destructured
+                if(ampm=="오전")
+                    ticker1.value=0
+                else
+                    ticker1.value=1
+                ticker2.value=hour.toInt()
+                ticker3.value=minute.toInt()/5
             } else {
-                hour - 12
-            }
-            view.text = "${AM_PM_start} ${hourStart}:${minute}"
-        }
-
-        timeStart.setOnClickListener {
-            if(timepickerStart.isVisible){
-                timepickerStart.isGone = true
-            }
-            else{
-                timepickerEnd.isGone = true
-                timepickerStart.isVisible = true
+                ticker1.value=0
+                ticker2.value=11
+                ticker3.value=0
             }
         }
 
-        timeEnd.setOnClickListener {
-            if(timepickerEnd.isVisible){
-                timepickerEnd.isGone = true
-            }
-            else{
-                timepickerStart.isGone = true
-                timepickerEnd.isVisible = true
+
+        ticker1.minValue = 0
+        ticker1.maxValue = 1
+        ticker1.displayedValues  = data1
+        ticker3.minValue = 0
+        ticker3.maxValue = 11
+        ticker3.displayedValues  = data2
+        ticker1.setOnValueChangedListener { picker, oldVal, newVal ->
+            if(newVal==0) {
+                times[scheduleSelect].text = times[scheduleSelect].text.toString().replace("오후","오전")
+            } else {
+                times[scheduleSelect].text = times[scheduleSelect].text.toString().replace("오전","오후")
             }
         }
-
-        timepickerStart.setOnTimeChangedListener { view, hourOfDay, minute ->
-            timeClick(hourOfDay, minute, timeStart)
+        ticker2.setOnValueChangedListener { picker, oldVal, newVal ->
+            val matchResult = regex.find(times[scheduleSelect].text.toString())
+            if (matchResult != null) {
+                val (ampm, hour, minute) = matchResult.destructured
+                times[scheduleSelect].text = "  "+ampm+" "+newVal+":"+minute+"  "
+            } else {
+            }
         }
-
-        timepickerEnd.setOnTimeChangedListener { view, hourOfDay, minute ->
-            timeClick(hourOfDay, minute, timeEnd)
+        ticker3.setOnValueChangedListener { picker, oldVal, newVal ->
+            val matchResult = regex.find(times[scheduleSelect].text.toString())
+            if (matchResult != null) {
+                val (ampm, hour, minute) = matchResult.destructured
+                times[scheduleSelect].text = "  "+ampm+" "+hour+":"+data2[newVal-1]+"  "
+            } else {
+            }
         }
 
         //등록 btn
