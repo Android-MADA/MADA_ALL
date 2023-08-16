e8f8faf1714e5681ca93ae4535d3f3f9368a203a
YUNSO99 <l01090259914@gmail.com>
2023-08-16 16:43:15 +0900

    초기화 버튼 설정

--- app/src/main/java/com/example/myapplication/CustomFunction/CustomBottomSheetViewPagar.kt
+++ app/src/main/java/com/example/myapplication/CustomFunction/CustomBottomSheetViewPagar.kt
@@ -7,12 +7,14 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
 import com.example.myapplication.Adpater.CustompageAdapter
 import com.example.myapplication.Fragment.FragCustom
 import java.lang.reflect.Field
 
 class  CustomBottomSheetViewPager (fragment : FragCustom) : FragmentStateAdapter(fragment){
     override fun getItemCount(): Int = 4
+    private val registeredFragments = mutableMapOf<Int, Fragment>()
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
@@ -21,5 +23,9 @@ class  CustomBottomSheetViewPager (fragment : FragCustom) : FragmentStateAdapter
             2 -> custom_item()
             else -> custom_background()
         }
+    }
+
+    fun getFragmentAtPosition(position: Int): Fragment? {
+        return registeredFragments[position]
     }
 }
