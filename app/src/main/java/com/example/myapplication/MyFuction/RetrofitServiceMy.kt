f76ed37a333952fde61b10714db8720aad9f2c76
Yewooony <0925jamie@gmail.com>
2023-08-19 01:46:16 +0900

    Yezi- 로그아웃, 회원탈퇴 서버 연결, 멘트 배경 삽입

--- app/src/main/java/com/example/myapplication/MyFuction/RetrofitServiceMy.kt
+++ app/src/main/java/com/example/myapplication/MyFuction/RetrofitServiceMy.kt
@@ -3,19 +3,30 @@ package com.example.myapplication.MyFuction
 import com.example.myapplication.MyFuction.Model.MyGetProfileData
 import retrofit2.http.GET
 import retrofit2.Call
+import retrofit2.http.DELETE
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 
 interface RetrofitServiceMy {
 
-    // 로그인 -> 확인 필요
-    @GET("/oauth2/authorization/naver")
-    fun login() : Call<Void>
+//    // 로그인 -> 필요 없음
+//    @GET("/oauth2/authorization/naver")
+//    fun login() : Call<Void>
 
-    // 프로필 편집창 닉네임, 이메일 조회
+    // 로그아웃 -> 확인
+    @GET("/user/logout")
+    fun logout(@Header("Authorization") token: String?
+    ) : Call<Void>
+
+    // 회원탈퇴 -> 확인
+    @DELETE("/user/withdrawal")
+    fun withdraw(@Header("Authorization") token: String?
+    ) : Call<Void>
+
+    // 프로필 편집창 닉네임, 이메일 불러오기 -> 확인
     @GET("/user/profile/change")
     fun myGetProfile(@Header("Authorization") token : String?
     ): Call<MyGetProfileData>
-
 
 }
