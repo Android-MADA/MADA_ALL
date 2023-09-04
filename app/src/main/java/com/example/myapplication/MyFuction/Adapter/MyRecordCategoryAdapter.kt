d869e1544b90663e704adb820699f4c88a130181
Yewooony <0925jamie@gmail.com>
2023-09-05 03:45:27 +0900

    Yezi- record 카테고리 리사이클러뷰 구현

--- app/src/main/java/com/example/myapplication/MyFuction/Adapter/MyRecordCategoryAdapter.kt
+++ app/src/main/java/com/example/myapplication/MyFuction/Adapter/MyRecordCategoryAdapter.kt
@@ -1,23 +1,23 @@
 package com.example.myapplication.MyFuction.Adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.myapplication.MyFuction.Data.MyRecordCategoryData
 import com.example.myapplication.R
 
 class MyRecordCategoryAdapter(private val context: Context) : RecyclerView.Adapter<MyRecordCategoryAdapter.ViewHolder>() {
 
     var datas = mutableListOf<MyRecordCategoryData>()
-        set(value) {
-            field = value
-            notifyDataSetChanged() // 데이터가 변경되었음을 알려 리사이클러뷰를 다시 그립니다.
-        }
+//        set(value) {
+//            field = value
+//            notifyDataSetChanged() // 데이터가 변경되었음을 알려 리사이클러뷰를 다시 그립니다.
+//        }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.my_record_list,parent,false)
         return ViewHolder(view)
@@ -33,11 +33,11 @@ class MyRecordCategoryAdapter(private val context: Context) : RecyclerView.Adapt
 
         private val txtPercent: TextView = itemView.findViewById(R.id.record_category_percent)
         private val txtCategory: TextView = itemView.findViewById(R.id.record_category_name)
-        private val imgColor: ImageView = itemView.findViewById(R.id.record_category_color)
+        private val viewColor: CardView = itemView.findViewById(R.id.record_category_color)
 
         fun bind(item: MyRecordCategoryData) {
             txtPercent.text = item.percent
-            Glide.with(itemView).load(item.colorImage).into(imgColor)
+            //viewColor.setCardBackgroundColor(Color.parseColor(item.colorCode))
             txtCategory.text = item.category
         }
     }
