fc6159eed83553a462833f6bf86112876442004d
Minji Kang <1117minsky@hufs.ac.kr>
2023-09-06 03:42:38 +0900

    [mindy] cate 서버 연결 완료

--- app/src/main/java/com/example/myapplication/HomeFunction/Model/Category.kt
+++ app/src/main/java/com/example/myapplication/HomeFunction/Model/Category.kt
@@ -6,10 +6,14 @@ data class Category(
     @SerializedName("id") val id: Int,
     @SerializedName("categoryName") var categoryName: String,
     @SerializedName("color") var color: String,
-    @SerializedName("iconId") var iconId : Int,
-    @SerializedName("isInActive") var isInActive : Boolean
+    @SerializedName("iconId") var iconId: Int,
+    @SerializedName("isInActive") var isInActive: Boolean
 )
 
-data class Category2 (
-    @SerializedName("id") val id: Int
-        )
+data class Category2(
+    @SerializedName("id") val id: Int,
+    @SerializedName("categoryName") var categoryName: String,
+    @SerializedName("color") var color: String,
+    @SerializedName("iconId") var iconId: Int,
+    @SerializedName("isInActive") var isInActive: Boolean
+)
