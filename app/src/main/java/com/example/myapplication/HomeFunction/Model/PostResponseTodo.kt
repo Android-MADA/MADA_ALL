0126b9cfec326e4a797fc8433457f9d53e4fba85
KangMinji <1117minsky@hufs.ac.kr>
2023-09-21 10:26:18 +0900

    Refactor : Home Model 구조 수정

--- app/src/main/java/com/example/myapplication/HomeFunction/Model/PostResponseTodo.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.HomeFunction.Model
-
-import com.google.gson.annotations.SerializedName
-
-data class PostResponseTodo(
-    @SerializedName("data") val data : todoData
-)
