8cc640b15ab0c0503f862ba5a846222c49009c9b
KangMinji <1117minsky@hufs.ac.kr>
2023-09-21 10:26:18 +0900

    Refactor : Home Model 구조 수정

--- app/src/main/java/com/example/myapplication/HomeFunction/Model/PostRequestTodoCateId.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.HomeFunction.Model
-
-import com.google.gson.annotations.SerializedName
-
-data class PostRequestTodoCateId(
-    @SerializedName("id") val id: Int
-)
--- app/src/main/java/com/example/myapplication/HomeFunction/Model/TodoList.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.HomeFunction.Model
-
-import com.google.gson.annotations.SerializedName
-
-data class TodoList(
-    //@SerializedName("data") var data: ArrayList<Todo>
-    @SerializedName("data") var data: todoData2
-
-)
