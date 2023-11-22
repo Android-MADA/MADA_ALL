db019771efb4b94f963fee47702605ac28b2b8cb
KangMinji <1117minsky@hufs.ac.kr>
2023-11-23 01:49:32 +0900

    Remove : Todo viewpager 삭제

--- app/src/main/java/com/example/myapplication/HomeFunction/adapter/todo/HomeViewPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/HomeFunction/adapter/todo/HomeViewPagerAdapter.kt
@@ -2,9 +2,6 @@ package com.example.myapplication.HomeFunction.adapter.todo
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.myapplication.Fragment.FragTime
-import com.example.myapplication.HomeFunction.view.HomeViewpagerDdayFragment
-import com.example.myapplication.HomeFunction.view.HomeViewpagerTodoFragment
 
 class HomeViewPagerAdapter(f: Fragment) : FragmentStateAdapter(f) {
 
