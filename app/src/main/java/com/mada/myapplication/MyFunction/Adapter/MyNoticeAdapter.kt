6f7b0b0cc3e685a6729eb45d87cb0d965d2e4d49
yezi <0925jamie@gmail.com>
2024-07-15 21:17:06 +0900

    Fix: 공지사항 리사이클러뷰로 수정

---
+++ app/src/main/java/com/mada/myapplication/MyFunction/Adapter/MyNoticeAdapter.kt
@@ -0,0 +1,31 @@
+package com.mada.myapplication.MyFunction.Adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mada.myapplication.databinding.MyNoticeItemBinding
+
+data class Notice(val title: String, val content: String, val date: String)
+class MyNoticeAdapter(private val noticeList: List<Notice>) :
+
+    RecyclerView.Adapter<MyNoticeAdapter.MyViewHolder>() {
+
+    class MyViewHolder(private val binding: MyNoticeItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(notice: Notice) {
+            binding.title.text = notice.title
+            binding.content.text = notice.content
+            binding.date.text = notice.date
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = MyNoticeItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(noticeList[position])
+    }
+
+    override fun getItemCount() = noticeList.size
+}
--- app/src/main/java/com/mada/myapplication/MyFunction/Fragment/MyNoticeFragment.kt
+++ app/src/main/java/com/mada/myapplication/MyFunction/Fragment/MyNoticeFragment.kt
@@ -5,17 +5,20 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mada.myapplication.HomeFunction.api.RetrofitInstance
+import com.mada.myapplication.MyFunction.Adapter.MyNoticeAdapter
+import com.mada.myapplication.MyFunction.Adapter.Notice
 import com.mada.myapplication.MyFunction.Data.MyGetNoticesData
 import com.mada.myapplication.MyFunction.RetrofitServiceMy
 import com.mada.myapplication.R
 import com.mada.myapplication.StartFunction.Splash2Activity
 import com.mada.myapplication.databinding.MyNoticeBinding
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.Response
 
 class MyNoticeFragment : Fragment() {
@@ -43,7 +46,11 @@ class MyNoticeFragment : Fragment() {
         }
 
         // 서버에서 공지사항 불러오기
-        api.myGetNotices(token).enqueue(object : retrofit2.Callback<MyGetNoticesData> {
+        fetchNotices()
+    }
+
+    private fun fetchNotices() {
+        api.myGetNotices(token).enqueue(object : Callback<MyGetNoticesData> {
             override fun onResponse(
                 call: Call<MyGetNoticesData>,
                 response: Response<MyGetNoticesData>
@@ -53,32 +60,10 @@ class MyNoticeFragment : Fragment() {
 
                 if (response.isSuccessful) {
                     Log.d("GetNotices 성공", response.body().toString())
-                    if(response.body()!!.data.isNotEmpty()){
-                        when (id) {
-                            0 -> {
-                                binding.title1.text = response.body()!!.data[1].title
-                                binding.content1.text = response.body()!!.data[1].content
-                                binding.date1.text = response.body()!!.data[1].date
-                            }
-                            1 -> {
-                                binding.title2.text = response.body()!!.data[2].title
-                                binding.content2.text = response.body()!!.data[2].content
-                                binding.date2.text = response.body()!!.data[2].date
-                            }
-                            // 배열크기 받아서 수정 필요
-                        }
-                    }
-                    else{
-                        Log.d("GetNotices", "DB가 비어있음")
-                        binding.title1.isVisible = false
-                        binding.title2.isVisible = false
-                        binding.content1.isVisible = false
-                        binding.content2.isVisible = false
-                        binding.date1.isVisible = false
-                        binding.date2.isVisible = false
-                        binding.divider4.isVisible = false
-                        binding.divider5.isVisible = false
-                    }
+
+                    val dataList = response.body()?.data?.map { Notice(it.title, it.content, it.date) } ?: emptyList()
+
+                    setupRecyclerView(dataList)
 
                 } else {
                     Log.d("GetNotices 실패", response.body().toString())
@@ -89,5 +74,11 @@ class MyNoticeFragment : Fragment() {
                 Log.d("서버 오류", "GetNotices 실패")
             }
         })
+    }
+
+    private fun setupRecyclerView(noticeList: List<Notice>) {
+        val adapter = MyNoticeAdapter(noticeList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
     }
 }
